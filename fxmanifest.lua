14f25a6c6044883d85e683e68b239cc78e50aa86
Artmines <arturowells@gmail.com>
2026-04-19 21:51:11 -0230

    version bump

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,7 @@ lua54 'yes'
 game 'gta5'
 name 'ox_inventory'
 author 'Overextended'
-version '2.47.6'
+version '2.47.7'
 repository 'https://github.com/Mythic-Framework/ox_inventory'
 description 'Slot-based inventory with item metadata support'
 
